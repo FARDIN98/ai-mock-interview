074679c32108dc7d55d2bb2a5578459a73bc07ee
FARDIN98 <fardinahmed66@gmail.com>
2025-04-04 01:12:28 +0600

    generated interviews on the homepage

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -16,7 +16,7 @@ import FormField from "./FormField";
 import { useRouter } from "next/navigation";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "@/firebase/client";
-import { signup, SignIn as signIn } from "@/lib/actions/auth.action";
+import { signUp, signIn } from "@/lib/actions/auth.action";
 
 
 const authFormSchema = (type: FormType) => {
@@ -46,7 +46,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
       if(type === "sign-up"){
         const {name, email, password} = values;
         const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
-        const result = await signup({
+        const result = await signUp({
           uid: userCredentials.user.uid,
           name: name!,
           email,
