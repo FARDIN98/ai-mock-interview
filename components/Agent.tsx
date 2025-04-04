084eb4ec709968259dbe97cc646419f857a3ec5d
FARDIN98 <fardinahmed66@gmail.com>
2025-04-05 04:33:29 +0600

    chnaged user image

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -212,7 +212,7 @@ const Agent = ({ userName, userId, type, interviewId, questions }: AgentProps) =
             {/* User information display */}
             <div className="card-border">
                 <div className="card-content">
-                    <Image src="/Fardin.jpg" alt="user avatar" width={540} height={540} className="rounded-full object-cover size-[120px]" />
+                    <Image src="/user-avatar.png" alt="user avatar" width={540} height={540} className="rounded-full object-cover size-[120px]" />
                     <h3>{userName}</h3>
                 </div>
             </div>
