996c5f23a7c07a24b6b6d27f2b53fa64993d2738
FARDIN98 <fardinahmed66@gmail.com>
2025-04-06 01:56:44 +0600

    implemented some performance optimization

--- next.config.ts
+++ next.config.ts
@@ -7,14 +7,57 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  compress: true,
+  poweredByHeader: false,
   images: {
     remotePatterns: [{
       protocol: 'https',
       hostname: 'lh3.googleusercontent.com',
       port: '',
       pathname: '/**'
-    }]
-  }
+    }],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    formats: ['image/webp'],
+    minimumCacheTTL: 60
+  },
+  headers: async () => [
+    {
+      source: '/:all*(svg|jpg|png)',
+      locale: false,
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'public, max-age=31536000, must-revalidate'
+        }
+      ]
+    },
+    {
+      source: '/:path*',
+      headers: [
+        {
+          key: 'X-DNS-Prefetch-Control',
+          value: 'on'
+        },
+        {
+          key: 'Strict-Transport-Security',
+          value: 'max-age=31536000; includeSubDomains'
+        },
+        {
+          key: 'X-Frame-Options',
+          value: 'SAMEORIGIN'
+        },
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff'
+        },
+        {
+          key: 'Referrer-Policy',
+          value: 'strict-origin-when-cross-origin'
+        }
+      ]
+    }
+  ]
 };
 
 export default nextConfig;
